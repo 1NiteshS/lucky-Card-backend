0c05cb71603d4e1a3d8d74be0fb2c0eaad3327a7
Nitish <nitishsingh@midknightstudio.com>
2024-10-17 14:53:17 +0530

    Not doing percentage work

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,6 +6,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { sendOTP } from '../utils/emailService.js';
 import Game from '../models/gameModel.js';
 import SelectedCard from '../models/selectedCardModel.js'
+import AdminWinnings from '../models/AdminWinnings.js'
 
 const generateOTP = () => {
   return Math.floor(100000 + Math.random() * 900000).toString();
